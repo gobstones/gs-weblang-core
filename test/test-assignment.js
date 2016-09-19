c6ed22e7a87e9eaa2807ceb8acb999e219969a76
AlvarezAriel <alvarez.ariel.work@gmail.com>
2016-09-19 00:41:49 -0300

    More assignment tests

--- test/test-assignment.js
+++ test/test-assignment.js
@@ -13,3 +13,5 @@ testAssignment('a := True', 'a', true);
 testAssignment('a := 1 + 2', 'a', 3);
 testAssignment('a := 1 + 2; a := 5', 'a', 5);
 testAssignment('a := 1 + 2; b := 5; a := b', 'a', 5);
+testAssignment('a := 1; c := 2; b := 3; b := c; a := b', 'a', 2);
+testAssignment('c := 2; b := 3; a := b + c', 'a', 5);
