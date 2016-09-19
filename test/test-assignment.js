ff0aab9b4f43c8b2ac830a90e3c2f7756f391a4a
AlvarezAriel <alvarez.ariel.work@gmail.com>
2016-09-19 00:39:22 -0300

    Evaluation for variable reference expression

--- test/test-assignment.js
+++ test/test-assignment.js
@@ -12,3 +12,4 @@ testAssignment('a := 2', 'a', 2);
 testAssignment('a := True', 'a', true);
 testAssignment('a := 1 + 2', 'a', 3);
 testAssignment('a := 1 + 2; a := 5', 'a', 5);
+testAssignment('a := 1 + 2; b := 5; a := b', 'a', 5);
