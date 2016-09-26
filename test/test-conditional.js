7cf887baf2078497f7894a8cfd5108eb66564f13
AlvarezAriel <alvarez.ariel.work@gmail.com>
2016-09-26 14:12:54 -0300

    Added tests for switch statement

--- test/test-conditional.js
+++ test/test-conditional.js
@@ -12,3 +12,10 @@ utils.testStatements('if', 'if(2 < 3 + 4){a := 1} else {a := 2}', 'a', 1);
 utils.testStatements('if', 'if(2 < 3 + 4 && True ){a := 1} else {a := 2}', 'a', 1);
 utils.testStatements('if', 'if(2 < 3 + 4 && False ){a := 1} else {a := 2}', 'a', 2);
 utils.testStatements('if', 'if(2 < 3 + 4 && 5 >= 5 ){a := 1} else {a := 2}', 'a', 1);
+
+utils.testStatements('switch', 'switch(1+2) { 1 -> { a := 1 }  2 -> { a:= 2 } 3 -> { a := 3 } }', 'a', 3);
+utils.testStatements('switch', 'switch(1+2) { 1 -> a := 1  2 -> a:= 2 3 -> a := 3 }', 'a', 3);
+utils.testStatements('switch', 'switch(1+2) { 1 -> a := 1  2 -> a:= 2 4 - 1 -> a := 3 }', 'a', 3);
+utils.testStatements('switch', 'switch(1+2) { 1 -> a := 1 4 - 1 -> a := 3 2 -> a:= 2 }', 'a', 3);
+utils.testStatements('switch', 'switch(Azul) { Negro -> a := 1  Azul -> a:= 2 Rojo -> a := 3 }', 'a', 2);
+// TODO: qué debe ocurrir cuando más de un caso es verdadero?
