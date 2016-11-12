3f8dd83d478181e6321855fa057208e022730a1c
AlvarezAriel <alvarez.ariel.work@gmail.com>
2016-11-12 07:55:24 -0300

    Added tests for functions with arguments

--- test/test-parse-big-program.js
+++ test/test-parse-big-program.js
@@ -3,3 +3,10 @@ var utils = require('./_utils');
 utils.testProgram('big-program.gbs', function (t, context) {
     t.is(context.get('a'), 3);
 });
+
+utils.testProgram('functions-with-arguments.gbs', function (t, context) {
+    t.is(context.get('a'), 6);
+    t.is(context.get('b'), 4);
+    t.is(context.get('c'), 3);
+    t.is(context.get('d'), 7);
+});
