3eed9a81a2b163682468592752c1067abf39a492
Rodrigo Alfonso <rodri042@gmail.com>
2016-11-29 12:11:53 -0300

    Throwing errors when unexpected constants appear

--- test/test-failures.js
+++ test/test-failures.js
@@ -9,3 +9,14 @@ utils.testProgramFailure('unknown-function.gbs', function (t, reason) {
     t.is(reason.message, 'La función "boom" no se encuentra definida.');
     t.deepEqual(reason.on.range.start, {row: 1, column: 10});
 });
+
+var testUnexpectedConstant = function (fileName) {
+    utils.testProgramFailure(fileName, function (t, reason) {
+        t.is(reason.message, 'Identificador "CualquierCosa" inesperado');
+        t.deepEqual(reason.on.range.start, {row: 1, column: 11});
+    });
+};
+
+testUnexpectedConstant('unknown-constant-in-Poner.gbs');
+testUnexpectedConstant('unknown-constant-in-Sacar.gbs');
+testUnexpectedConstant('unknown-constant-in-Mover.gbs');
