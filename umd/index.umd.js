a3f9f1a95e7faf204be30786b06d2647ce920040
Rodrigo Alfonso <rodri042@gmail.com>
2017-05-09 22:07:27 -0300

    Bump 1.9.4

--- umd/index.umd.js
+++ umd/index.umd.js
@@ -510,7 +510,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	var ARROW_KEYS = ['ARROW_LEFT', 'ARROW_RIGHT', 'ARROW_UP', 'ARROW_DOWN'];
 	var SPECIAL_KEYS = ['SPACE', 'ENTER', 'TAB', 'BACKSPACE', 'DELETE', 'ESCAPE'];
 	var SYMBOL_KEYS = ['PLUS', 'MINUS', 'ASTERISK', 'SLASH', 'EQUALS', 'L_PARENT', 'R_PARENT', 'L_BRACKET', 'R_BRACKET', 'L_ANGLEBR', 'R_ANGLEBR'].concat(ARROW_KEYS);
-	var MODIFIERS = ['CTRL', 'ALT', 'SHIFT', 'CTRL_ALT', 'CTRL_SHIFT', 'ALT_SHIFT', 'CTRL_ALT_SHIFT'];
+	var MODIFIERS = ['CTRL_ALT_SHIFT', 'ALT_SHIFT', 'CTRL_ALT', 'CTRL_SHIFT', 'CTRL', 'ALT', 'SHIFT'];
 	
 	var isUpperChar = function (char) {
 	    return /^[A-ZÑÁÉÍÓÚ]$/.test(char);
