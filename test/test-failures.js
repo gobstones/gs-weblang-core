c56c61687db1f748175cb33dc30d1ff79379a36c
Rodrigo Alfonso <rodri042@gmail.com>
2017-05-22 19:11:56 -0300

    Making the tests less coupled to the locales

--- test/test-failures.js
+++ test/test-failures.js
@@ -1,111 +1,101 @@
+var _ = require('lodash');
 var utils = require('./_utils');
 
-utils.testProgramFailure('boom.gbs', function (t, reason) {
-    t.is(reason.message, 'El procedimiento "Boomba" no se encuentra definido.');
-    t.deepEqual(reason.reason, {code: 'undefined_procedure', detail: {name: 'Boomba'}});
-    t.deepEqual(reason.on.range.start, {row: 1, column: 5});
+var messageIncludeAllDetail = function (t, error) {
+    t.is(_.values(error.reason.detail).every(function (value) {
+        return _.includes(error.message, value);
+    }), true);
+};
+
+utils.testProgramFailure('boom.gbs', function (t, error) {
+    messageIncludeAllDetail(t, error);
+    t.deepEqual(error.reason, {code: 'undefined_procedure', detail: {name: 'Boomba'}});
+    t.deepEqual(error.on.range.start, {row: 1, column: 5});
 });
 
-utils.testProgramFailure('unknown-function.gbs', function (t, reason) {
-    t.is(reason.message, 'La función "boom" no se encuentra definida.');
-    t.deepEqual(reason.reason, {code: 'undefined_function', detail: {name: 'boom'}});
-    t.deepEqual(reason.on.range.start, {row: 1, column: 10});
+utils.testProgramFailure('unknown-function.gbs', function (t, error) {
+    messageIncludeAllDetail(t, error);
+    t.deepEqual(error.reason, {code: 'undefined_function', detail: {name: 'boom'}});
+    t.deepEqual(error.on.range.start, {row: 1, column: 10});
 });
 
-utils.testProgramFailure('unknown-function.gbs', function (t, reason) {
-    t.is(reason.message, 'La función "boom" no se encuentra definida.');
-    t.deepEqual(reason.reason, {code: 'undefined_function', detail: {name: 'boom'}});
-    t.deepEqual(reason.on.range.start, {row: 1, column: 10});
+utils.testProgramFailure('unknown-function.gbs', function (t, error) {
+    messageIncludeAllDetail(t, error);
+    t.deepEqual(error.reason, {code: 'undefined_function', detail: {name: 'boom'}});
+    t.deepEqual(error.on.range.start, {row: 1, column: 10});
 });
 
-utils.testProgramFailure('returning-non-numeric-exit-code.gbs', function (t, reason) {
-    t.is(reason.message, 'El programa retornó un valor no numérico.');
-    t.deepEqual(reason.reason, {code: 'non_numeric_exit_code', detail: [0, 1]});
-    t.deepEqual(reason.on.range.start, {row: 3, column: 11});
+utils.testProgramFailure('returning-non-numeric-exit-code.gbs', function (t, error) {
+    t.is(error.message !== '', true);
+    t.deepEqual(error.reason, {code: 'non_numeric_exit_code', detail: {value: [0, 1]}});
+    t.deepEqual(error.on.range.start, {row: 3, column: 11});
 });
 
-utils.testProgramFailure('wrong-types/basic-move-red.gbs', function (t, reason) {
-    t.is(reason.message, 'Se esperaba un valor de tipo "Dirección" pero se encontró uno de tipo "Color".');
-    t.deepEqual(reason.reason, {code: 'type_mismatch', detail: {expected: 'Dirección', actual: 'Color'}});
-    t.deepEqual(reason.on.range.start, {row: 1, column: 10});
+utils.testProgramFailure('wrong-types/basic-move-red.gbs', function (t, error) {
+    messageIncludeAllDetail(t, error);
+    t.deepEqual(error.reason, {code: 'type_mismatch', detail: {expected: 'Dirección', actual: 'Color'}});
+    t.deepEqual(error.on.range.start, {row: 1, column: 10});
 });
 
-utils.testProgramFailure('wrong-types/move-red.gbs', function (t, reason) {
-    t.is(reason.message, 'Se esperaba un valor de tipo "Dirección" pero se encontró uno de tipo "Color".');
-    t.deepEqual(reason.reason, {code: 'type_mismatch', detail: {expected: 'Dirección', actual: 'Color'}});
-    t.deepEqual(reason.on.range.start, {row: 5, column: 10});
+utils.testProgramFailure('wrong-types/move-red.gbs', function (t, error) {
+    messageIncludeAllDetail(t, error);
+    t.deepEqual(error.reason, {code: 'type_mismatch', detail: {expected: 'Dirección', actual: 'Color'}});
+    t.deepEqual(error.on.range.start, {row: 5, column: 10});
 });
 
-utils.testProgramFailure('wrong-types/move-red-by-parameter.gbs', function (t, reason) {
-    t.is(reason.message, 'Se esperaba un valor de tipo "Dirección" pero se encontró uno de tipo "Color".');
-    t.deepEqual(reason.reason, {code: 'type_mismatch', detail: {expected: 'Dirección', actual: 'Color'}});
-    t.deepEqual(reason.on.range.start, {row: 1, column: 10});
+utils.testProgramFailure('wrong-types/move-red-by-parameter.gbs', function (t, error) {
+    messageIncludeAllDetail(t, error);
+    t.deepEqual(error.reason, {code: 'type_mismatch', detail: {expected: 'Dirección', actual: 'Color'}});
+    t.deepEqual(error.on.range.start, {row: 1, column: 10});
 });
 
-utils.testProgramFailure('wrong-types/put-north.gbs', function (t, reason) {
-    t.is(reason.message, 'Se esperaba un valor de tipo "Color" pero se encontró uno de tipo "Dirección".');
-    t.deepEqual(reason.reason, {code: 'type_mismatch', detail: {expected: 'Color', actual: 'Dirección'}});
-    t.deepEqual(reason.on.range.start, {row: 5, column: 10});
+utils.testProgramFailure('wrong-types/put-north.gbs', function (t, error) {
+    messageIncludeAllDetail(t, error);
+    t.deepEqual(error.reason, {code: 'type_mismatch', detail: {expected: 'Color', actual: 'Dirección'}});
+    t.deepEqual(error.on.range.start, {row: 5, column: 10});
 });
 
-utils.testProgramFailure('wrong-types/numberofstones-east.gbs', function (t, reason) {
-    t.is(reason.message, 'Se esperaba un valor de tipo "Color" pero se encontró uno de tipo "Dirección".');
-    t.deepEqual(reason.reason, {code: 'type_mismatch', detail: {expected: 'Color', actual: 'Dirección'}});
-    t.deepEqual(reason.on.range.start, {row: 1, column: 20});
+utils.testProgramFailure('wrong-types/numberofstones-east.gbs', function (t, error) {
+    messageIncludeAllDetail(t, error);
+    t.deepEqual(error.reason, {code: 'type_mismatch', detail: {expected: 'Color', actual: 'Dirección'}});
+    t.deepEqual(error.on.range.start, {row: 1, column: 20});
 });
 
-utils.testProgramFailure('wrong-types/opposite-red.gbs', function (t, reason) {
-    t.is(reason.message, 'Se esperaba un valor de tipo "Dirección" pero se encontró uno de tipo "Color".');
-    t.deepEqual(reason.reason, {code: 'type_mismatch', detail: {expected: 'Dirección', actual: 'Color'}});
-    t.deepEqual(reason.on.range.start, {row: 1, column: 17});
+utils.testProgramFailure('wrong-types/opposite-red.gbs', function (t, error) {
+    messageIncludeAllDetail(t, error);
+    t.deepEqual(error.reason, {code: 'type_mismatch', detail: {expected: 'Dirección', actual: 'Color'}});
+    t.deepEqual(error.on.range.start, {row: 1, column: 17});
 });
 
-utils.testProgramFailure('wrong-types/inconsistent-assignment-1.gbs', function (t, reason) {
-    t.is(reason.message, 'No se puede asignar a "a" un valor de tipo "Dirección" ya que es de tipo "Número".');
-    t.deepEqual(reason.reason, {code: 'inconsistent_assignment', detail: {name: 'a', expected: 'Número', actual: 'Dirección'}});
-    t.deepEqual(reason.on.range.start, {row: 2, column: 5});
+utils.testProgramFailure('wrong-types/inconsistent-assignment-1.gbs', function (t, error) {
+    messageIncludeAllDetail(t, error);
+    t.deepEqual(error.reason, {code: 'inconsistent_assignment', detail: {name: 'a', expected: 'Número', actual: 'Dirección'}});
+    t.deepEqual(error.on.range.start, {row: 2, column: 5});
 });
 
-utils.testProgramFailure('wrong-types/inconsistent-assignment-2.gbs', function (t, reason) {
-    t.is(reason.message, 'No se puede asignar a "a" un valor de tipo "Color" ya que es de tipo "Dirección".');
-    t.deepEqual(reason.reason, {code: 'inconsistent_assignment', detail: {name: 'a', expected: 'Dirección', actual: 'Color'}});
-    t.deepEqual(reason.on.range.start, {row: 2, column: 5});
+utils.testProgramFailure('wrong-types/inconsistent-assignment-2.gbs', function (t, error) {
+    messageIncludeAllDetail(t, error);
+    t.deepEqual(error.reason, {code: 'inconsistent_assignment', detail: {name: 'a', expected: 'Dirección', actual: 'Color'}});
+    t.deepEqual(error.on.range.start, {row: 2, column: 5});
 });
 
-utils.testProgramFailure('wrong-types/wrong-arity.gbs', function (t, reason) {
-    t.is(reason.message, 'Se esperaban 2 argumentos pero se obtuvieron 1.');
-    t.deepEqual(reason.reason, {code: 'wrong_arity', detail: {expected: 2, actual: 1}});
-    t.deepEqual(reason.on.range.start, {row: 6, column: 5});
+utils.testProgramFailure('wrong-types/wrong-arity.gbs', function (t, error) {
+    messageIncludeAllDetail(t, error);
+    t.deepEqual(error.reason, {code: 'wrong_arity', detail: {expected: 2, actual: 1}});
+    t.deepEqual(error.on.range.start, {row: 6, column: 5});
 });
 
-utils.testProgramFailure('wrong-types/inconsistent-switch-branch-types.gbs', function (t, reason) {
-    t.is(reason.message, 'Se esperaba un valor de tipo "Color" pero se encontró uno de tipo "Número".');
-    t.deepEqual(reason.reason, {code: 'type_mismatch', detail: {expected: 'Color', actual: 'Número'}});
-    t.deepEqual(reason.on.range.start, {row: 2, column: 9});
-});
-
-utils.testProgramFailure('user-boom/good-boom.gbs', function (t, reason) {
-    t.is(reason.message, 'Ahora sí se rompe todo');
-    t.is(reason.reason.code, 'boom_called');
-});
-
-utils.testProgramFailure('user-boom/without-parameters.gbs', function (t, reason) {
-    t.is(reason.error, 'Se esperaba un mensaje de error entre comillas');
-});
-
-utils.testProgramFailure('user-boom/with-color-as-parameter.gbs', function (t, reason) {
-    t.is(reason.error, 'Se esperaba un mensaje de error entre comillas');
-});
-
-utils.testProgramFailure('user-boom/with-no-closing-quote.gbs', function (t, reason) {
-    t.is(reason.error, 'Se esperaba un cierre de comillas');
+utils.testProgramFailure('wrong-types/inconsistent-switch-branch-types.gbs', function (t, error) {
+    messageIncludeAllDetail(t, error);
+    t.deepEqual(error.reason, {code: 'type_mismatch', detail: {expected: 'Color', actual: 'Número'}});
+    t.deepEqual(error.on.range.start, {row: 2, column: 9});
 });
 
 var testUnexpectedConstant = function (fileName) {
-    utils.testProgramFailure(fileName, function (t, reason) {
-        t.is(reason.message, 'El nombre "cualquierCosa" no existe.');
-        t.deepEqual(reason.reason, {code: 'undefined_name', detail: {name: 'cualquierCosa'}});
-        t.deepEqual(reason.on.range.start, {row: 1, column: 11});
+    utils.testProgramFailure(fileName, function (t, error) {
+        messageIncludeAllDetail(t, error);
+        t.deepEqual(error.reason, {code: 'undefined_name', detail: {name: 'cualquierCosa'}});
+        t.deepEqual(error.on.range.start, {row: 1, column: 11});
     });
 };
 
@@ -113,16 +103,37 @@ testUnexpectedConstant('unknown-constant-in-Poner.gbs');
 testUnexpectedConstant('unknown-constant-in-Sacar.gbs');
 testUnexpectedConstant('unknown-constant-in-Mover.gbs');
 
-utils.testProgramFailure('unknown-literal-in-Mover.gbs', function (t, reason) {
-    t.is(reason.message, 'El literal "CualquierCosa" no existe.');
-    t.deepEqual(reason.reason, {code: 'undefined_literal', detail: {name: 'CualquierCosa'}});
-    t.deepEqual(reason.on.range.start, {row: 1, column: 11});
+utils.testProgramFailure('unknown-literal-in-Mover.gbs', function (t, error) {
+    messageIncludeAllDetail(t, error);
+    t.deepEqual(error.reason, {code: 'undefined_literal', detail: {name: 'CualquierCosa'}});
+    t.deepEqual(error.on.range.start, {row: 1, column: 11});
 });
 
-utils.testProgramFailure('unknown-literal-in-any-procedure.gbs', function (t, reason) {
-    t.is(reason.message, 'El literal "CualquierCosa" no existe.');
-    t.deepEqual(reason.reason, {code: 'undefined_literal', detail: {name: 'CualquierCosa'}});
-    t.deepEqual(reason.on.range.start, {row: 6, column: 16});
+utils.testProgramFailure('unknown-literal-in-any-procedure.gbs', function (t, error) {
+    messageIncludeAllDetail(t, error);
+    t.deepEqual(error.reason, {code: 'undefined_literal', detail: {name: 'CualquierCosa'}});
+    t.deepEqual(error.on.range.start, {row: 6, column: 16});
+});
+
+// ---
+
+utils.testProgramFailure('user-boom/good-boom.gbs', function (t, error) {
+    t.is(error.message, 'Ahora sí se rompe todo');
+    t.is(error.reason.code, 'boom_called');
+});
+
+// ---
+
+utils.testProgramFailure('user-boom/without-parameters.gbs', function (t, error) {
+    t.is(error.error, 'Se esperaba un mensaje de error entre comillas');
+});
+
+utils.testProgramFailure('user-boom/with-color-as-parameter.gbs', function (t, error) {
+    t.is(error.error, 'Se esperaba un mensaje de error entre comillas');
+});
+
+utils.testProgramFailure('user-boom/with-no-closing-quote.gbs', function (t, error) {
+    t.is(error.error, 'Se esperaba un cierre de comillas');
 });
 
 utils.testProgramFailure('interactive/wrong-keys.gbs', function (t, reason) {
