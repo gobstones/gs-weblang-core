07edc5842a4af8e0b3444966211259a7ac778d50
Rodrigo Alfonso <rodri042@gmail.com>
2017-05-22 16:41:03 -0300

    Bump 1.10.5

--- umd/index.umd.js
+++ umd/index.umd.js
@@ -18532,12 +18532,15 @@ return /******/ (function(modules) { // webpackBootstrap
 	
 	        for (var i = 0; i < this.cases.length; i++) {
 	            var branchExp = wrap(this.cases[i].case.eval(context, {}));
-	            var token = branchExp.token.range ? branchExp.token : this.token;
-	            // TODO: ^- En algunos casos no se puede reportar el error bien porque los símbolos no tienen range
-	            // Ejemplo: program { switch (Rojo) { Verde -> Mover(Norte) Sur -> Mover(Este) } }
+	            var token = branchExp.token && branchExp.token.range ? branchExp.token : this.token;
+	            // TODO: ^- En algunos casos no se reporta el error bien (con el token correcto)
+	            // porque los símbolos o los valores `wrap`peados no tienen range.
+	            // Ejemplos:
+	            // program { switch (Rojo) { Verde -> Mover(Norte) Sur -> Mover(Este) } }
+	            // program { switch(Rojo) { 1+2 -> a := 1 2 -> a:= 2 } }
 	
 	            if (exp.type !== branchExp.type) {
-	                node.errors.throwTypeMismatch(this.cases[i].case.token, exp.type, branchExp.type);
+	                node.errors.throwTypeMismatch(token, exp.type, branchExp.type);
 	            }
 	
 	            if (branchExp.value === exp.value) {
