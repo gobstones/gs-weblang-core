8be3de70a90721271b8cc3fdc8d54b6ae5f3a624
Rodrigo Alfonso <rodri042@gmail.com>
2017-06-01 22:46:21 -0300

    Fixing the pacman bug: wrapping direction values

--- test/test-primitive-functions.js
+++ test/test-primitive-functions.js
@@ -1,8 +1,11 @@
 var utils = require('./_utils');
 
-utils.testProgram('opposite.gbs', function (t, context) {
+var boardIsIn10 = function (t, context) {
     var board = context.board();
 
     t.is(board.x, 1);
     t.is(board.y, 0);
-});
+};
+
+utils.testProgram('opposite.gbs', boardIsIn10);
+utils.testProgram('moving-opposite-direction-variable.gbs', boardIsIn10);
